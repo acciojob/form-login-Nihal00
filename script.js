64775f4a9ab447d1dd912f4b6378bb124e595115
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-18 17:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function getFormvalue() {
     //Write your code here
-	let firstName = document.getElementByName("fname");
-	let lastName = document.getElementByName("lname");
+	let firstName = document.getElementByName("fname").value;
+	let lastName = document.getElementByName("lname").value;
 
 	alert(firstName + " " + lastName);
 }
+
+// document.getElementsByTagName('input').addEventListner('click', getFormvalue);
